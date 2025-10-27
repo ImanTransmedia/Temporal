40e27f137f6243a18820a766ee230966cc85c681
Serghy Castillo <b1uefeet.dev@gmail.com>
2025-10-27 10:11:06 -0500

    Selection

--- Assets/Uzhupud/Scripts/VRSeleccionManager.cs
+++ Assets/Uzhupud/Scripts/VRSeleccionManager.cs
@@ -1,20 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
-// MANAGER: asegura seleccion unica y emite id
+[System.Serializable]
+public class IdScene
+{
+    public string id;
+    public string sceneName;
+}
+
 public class VRSeleccionManager : MonoBehaviour
 {
     public UnityEvent<string> OnSeleccionConId;
+    public GameObject uiConCarga;
+    public GameObject uiSinCarga;
+    public List<IdScene> idsConCarga = new List<IdScene>(); // agrega {id="2",sceneName="EscenaPiso2"} y {id="8",sceneName="EscenaPiso8"}
 
     private VRSelectable actual;
+    private string escenaPendiente;
 
     public void Seleccionar(VRSelectable nuevo)
     {
         if (actual == nuevo) return;
+
         if (actual != null) actual.SetActivo(false);
         actual = nuevo;
         if (actual != null) actual.SetActivo(true);
+
         OnSeleccionConId?.Invoke(nuevo.id);
+
+        var tieneCarga = TryGetSceneForId(nuevo.id, out escenaPendiente);
+        if (uiConCarga != null) uiConCarga.SetActive(tieneCarga);
+        if (uiSinCarga != null) uiSinCarga.SetActive(!tieneCarga);
     }
 
     public void Deseleccionar(VRSelectable item)
@@ -23,6 +41,42 @@ public class VRSeleccionManager : MonoBehaviour
         {
             actual.SetActivo(false);
             actual = null;
+            escenaPendiente = null;
+            OcultarUIs();
         }
+    }
+
+    public void Regresar()
+    {
+        if (actual != null) actual.SetActivo(false);
+        actual = null;
+        escenaPendiente = null;
+        OcultarUIs();
+    }
+
+    public void CargarEscenaSeleccionada()
+    {
+        if (!string.IsNullOrEmpty(escenaPendiente))
+            SceneManager.LoadScene(escenaPendiente);
+    }
+
+    private void OcultarUIs()
+    {
+        if (uiConCarga != null) uiConCarga.SetActive(false);
+        if (uiSinCarga != null) uiSinCarga.SetActive(false);
+    }
+
+    private bool TryGetSceneForId(string id, out string sceneName)
+    {
+        for (int i = 0; i < idsConCarga.Count; i++)
+        {
+            if (idsConCarga[i].id == id)
+            {
+                sceneName = idsConCarga[i].sceneName;
+                return true;
+            }
+        }
+        sceneName = null;
+        return false;
     }
 }
